ff57c2afa7ce4863ae3e716340c53ed176d867bd
csadhok-38 <admofficial62@gmail.com>
2024-02-24 18:12:07 +0530

    key changed

--- src/FetchVideos.js
+++ src/FetchVideos.js
@@ -5,7 +5,7 @@
 
 let FetchVideos =  async ()=>{
     
-    const apiKey = 'AIzaSyD8weTwiVeeE22DFs4sLqcW9vNGeEdz6fc';
+    const apiKey = '';
     const apiUrl = `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=40&key=${apiKey}`;
     const response = await fetch(apiUrl);
     const data = await response.json();
@@ -22,7 +22,7 @@ let FetchVideos =  async ()=>{
 
 let FetchSearchedVideos = async(searchQuery)=>{
 
-    const apiKey = 'AIzaSyD8weTwiVeeE22DFs4sLqcW9vNGeEdz6fc';
+    const apiKey = '';
     const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(searchQuery)}&type=video&maxResults=40&key=${apiKey}`;
     const response = await fetch(apiUrl);
     const data = await response.json();
